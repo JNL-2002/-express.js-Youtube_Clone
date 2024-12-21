dbc44b7f21f4b06a650ee47b165df2e4ae6c9f9a
JNL-2002 <jnl9327@naver.com>
2024-12-22 00:52:06 +0900

    searchMiddleware.js

--- middleware/searchMiddleware.js
+++ middleware/searchMiddleware.js
@@ -1,1 +1,33 @@
+const {spawn} = require('child_process');
 
+
+exports.search = (searchQuery) => {
+return new Promise((resolve, reject) => {
+  const process = spawn('python', ['search.py', searchQuery], {
+        encoding : "utf8"
+    });
+
+  let output;
+  let errData;
+
+  process.stdout.on('data', (data) => {
+    output = data;
+  });
+
+  process.stderr.on('data', (data) => {
+            errData = data; 
+  });
+
+  process.on('exit', (code) => {
+    if (code === 0) {
+      resolve(output) 
+    } else {
+      reject(errData)
+    }
+  });
+
+  process.on('error', (err) => {
+            reject(err);
+        });
+  });
+}
